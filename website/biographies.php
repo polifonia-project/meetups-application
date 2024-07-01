87404e67fd2877f91591417f9ec0a1b70aaccd55
JaseMK <jase@jasoncarvalho.com>
2024-07-01 16:56:01 +0100

    Added featured biographies - 6 static biogs to biographies landing page

--- website/biographies.php
+++ website/biographies.php
@@ -47,47 +47,255 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 mt-4 text-gray-800">Biographies</h1>
-                    <p class="mb-4">Biographies stored within the MEETUPS data are listed and summarised below.
-                        Select a biography to explore the subject in more detail and view the meetups that it describes. </p>
 
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <!--
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                        </div>
-                        -->
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Born</th>
-                                            <th>Died</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
 
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Born</th>
-                                            <th>Died</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-biog-featured" data-toggle="tab" data-target="#biog-featured"
+                                type="button" role="tab" aria-controls="biog-featured" aria-selected="true">Featured biographies
+                        </button>
+                        <button class="nav-link" id="nav-tabular-tab" data-toggle="tab" data-target="#tabular-tab"
+                                type="button" role="tab" aria-controls="tabular-tab" aria-selected="false">All biographies
+                        </button>
 
-                                        <tr>
-                                            <td>loading...</td>
-                                            <td>loading...</td>
-                                            <td>loading...</td>
-                                        </tr>
+                    </div>
+                </nav>
 
-                                    </tbody>
-                                </table>
+                <div class="tab-content">
+
+                    <div class="tab-pane fade show active" id="biog-featured" role="tabpanel" aria-labelledby="biog-featured-tab">
+                        <div class="card shadow mb-4">
+                            <!--<div class="card-header py-3">
+
+                            </div>-->
+                            <div class="card-body">
+                                <p>Explore a selection of featured biographies from the Musical Meetups Knowledge Graph. A full
+                                list of all biographies is available via the <em>All biographies</em> tab above.</p>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Arthur Sullivan <em>(63)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Sir_Arthur_Seymour_Sullivan.jpg?width=300" class="rounded float-right" width="100px">
+                                                    Sir Arthur Seymour Sullivan MVO (13 May 1842 – 22 November 1900) was an English composer.
+                                                    He is best known for 14 operatic collaborations with the dramatist W. S. Gilbert,
+                                                    including H.M.S. Pinafore, T...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Arthur_Sullivan">
+                                                        Explore biography and meetups for Arthur Sullivan
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Clara Schumann <em>(51)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Andreas_Staub_-_Clara_Schumann_(Pastell_1838).png?width=300" class="rounded float-right" width="100px">
+                                                    Clara Josephine Schumann was a German pianist, composer, and piano teacher. Regarded as one of the
+                                                    most distinguished pianists of the Romantic era, she exerted her influence over the course of a
+                                                    61-year concert career...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Clara_Schumann">
+                                                        Explore biography and meetups for Clara Schumann
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Wolfgang Amadeus Mozart <em>(35)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Wolfgang_Amadeus_Mozart_Signature.svg?width=300" class="rounded float-right" width="100px">
+                                                    Wolfgang Amadeus Mozart (27 January 1756 – 5 December 1791), baptised as Joannes Chrysostomus Wolfgangus
+                                                    Theophilus Mozart, was a prolific and influential composer of the Classical period. Despite his short life,
+                                                    his rapid pace of composition resulted in more than 800 works...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Wolfgang_Amadeus_Mozart">
+                                                        Explore biography and meetups for Wolfgang Amadeus Mozart
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Benjamin Britten <em>(88)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Benjamin_Britten,_London_Records_1968_publicity_photo_for_Wikipedia.jpg?width=300" class="rounded float-right" width="100px">
+                                                    Edward Benjamin Britten, was an English
+                                                    composer, conductor, and pianist. He was a central figure of 20th-century British music, with a range of works
+                                                    including opera, other vocal music, orchestral and chamber pieces. His best-known works include the opera Peter Grimes (1945)...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Benjamin_Britten">
+                                                        Explore biography and meetups for Benjamin Britten
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Edward Elgar <em>(85)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Edward_Elgar.jpg?width=300" class="rounded float-right" width="100px">
+                                                    Sir Edward William Elgar was an English composer, many of whose works have entered the British and
+                                                    international classical concert repertoire. Among his best-known compositions are orchestral
+                                                    works including the Enigma Variations, the Pomp and Circumstance Marches, concertos for violin...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Edward_Elgar">
+                                                        Explore biography and meetups for Edward Elgar
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <div class="card shadow mb-4 border-left-success">
+                                            <div class="card-header py-3">
+                                                <div class="float-right">
+                                                    <i class="fas fa-user text-success"></i>
+                                                </div>
+                                                <h6 class="m-0 font-weight-bold text-primary">
+                                                    Billie Holiday <em>(3)</em>
+                                                </h6>
+                                            </div>
+                                            <div class="card card-body">
+                                                <p>
+                                                    <img src="http://commons.wikimedia.org/wiki/Special:FilePath/(Portrait_of_Billie_Holiday_and_Mister,_Downbeat,_New_York,_N.Y.,_ca._Feb._1947)_(LOC)_(5020400274)_(cropped).jpg?width=300" class="rounded float-right" width="100px">
+                                                    Billie Holiday was an American jazz and swing music singer. Nicknamed "Lady Day" by her friend and music
+                                                    partner, Lester Young, Holiday had an innovative influence on jazz music and pop singing. Her vocal
+                                                    style, strongly inspired by jazz instrumentalists, pioneered a new way of manipulating phrasing and tempo...
+                                                </p>
+                                                <p>
+                                                    <a href="biography.php?id=http://dbpedia.org/resource/Billie_Holiday">
+                                                        Explore biography and meetups for Billie Holiday
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
+
+                    <div class="tab-pane fade show" id="tabular-tab" role="tabpanel" aria-labelledby="tabular-tab">
+                        <div class="card shadow mb-4">
+                            <!--<div class="card-header py-3">
+
+                            </div>-->
+                            <div class="card-body">
+                                <p class="mb-4">All biographies stored within the MEETUPS data are listed and summarised below.
+                                    Select a biography to explore the subject in more detail and view the meetups that it describes. </p>
+
+                                <!-- DataTales Example -->
+                                <div class="card shadow mb-4">
+                                    <!--
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                                    </div>
+                                    -->
+                                    <div class="card-body">
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                                <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Born</th>
+                                                    <th>Died</th>
+                                                </tr>
+                                                </thead>
+                                                <tfoot>
+
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Born</th>
+                                                    <th>Died</th>
+                                                </tr>
+                                                </tfoot>
+                                                <tbody>
+
+                                                <tr>
+                                                    <td>loading...</td>
+                                                    <td>loading...</td>
+                                                    <td>loading...</td>
+                                                </tr>
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
+
 
                 </div>
                 <!-- /.container-fluid -->
